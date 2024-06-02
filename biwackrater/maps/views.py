ea8f5619f1783b3a671b798f76fd8bcbaee86b26
MartinBartos <martin.bartos971014@gmail.com>
2024-06-02 19:51:32 +0200

    basic view of map

--- biwackrater/maps/views.py
+++ biwackrater/maps/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from folium import Map
 
 # Create your views here.
 
 class MapView:
     def init_maps(request):
-        return HttpResponse('Well, hello to biwackrater')
+        m = Map(location=[50.0755, 14.43784])
+        template = loader.get_template('maps/homepage.html')
+        context = {'my_map': m._repr_html_()}
+        return HttpResponse(template.render(context, request))
